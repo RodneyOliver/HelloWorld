1e456e3ec659e07ee2d0082f8c73cbc5c988cc9e
RodneyOliver <rodneywoliver@gmail.com>
2015-10-16 10:29:43 -0500

    Added Hello World Class
    Added a HelloWorld class object which includes a "main" as the driver.

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+
+
+
+
+public class HelloWorld 
+{
+
+	public static void main(String[] args) 
+	{
+		// Prints "Hello World" to the console output
+		
+		System.out.println("Hello World");
+		
+		// Right On!!, Simple starter project
+	}
+
+}
